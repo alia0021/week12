eaf5b3ad812e1fc3bd23a0ea708c38d9f2c6f3a8
Frah <alia0021@algonquinlive.com>
2019-08-02 12:10:22 -0400

    initial commit - auth complete

--- src/store.js
+++ src/store.js
@@ -1,10 +1,65 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import axiosAuth from "./axios-auth";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    idToken: null,
+    userId: null,
+    error: ""
+    // saves the authentication
+  },
+  mutations: {
+    AUTH_USER(state, userData) {
+      state.idToken = userData.token;
+      state.userId = userData.userId;
+    }
+  },
+  actions: {
+    signUp({ commit }, authData) {
+      axiosAuth
+        .post("accounts:signUp?key=AIzaSyCCH7id-AA19GQq5SCIrp_4nKqDOrPEYiQ", {
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        })
+        .then(res => {
+          console.log(res);
+          commit("AUTH_USER", {
+            token: res.data.idToken,
+            userId: res.data.localId
+          });
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(error.response.data.error.message);
+          }
+        });
+    },
+    signIn({ commit }, authData) {
+      axiosAuth
+        .post(
+          "accounts:signInWithPassword?key=AIzaSyCCH7id-AA19GQq5SCIrp_4nKqDOrPEYiQ",
+          {
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+          }
+        )
+        .then(res => {
+          console.log(res);
+          commit("AUTH_USER", {
+            token: res.data.idToken,
+            userId: res.data.localId
+          });
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(error.response.data.error.message);
+          }
+        });
+    }
+  }
 });
+//AIzaSyCCH7id-AA19GQq5SCIrp_4nKqDOrPEYiQ
